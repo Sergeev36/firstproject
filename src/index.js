92ca238a93b5004f7835e893610e328bfe0eece6
Sergeev36 <sergeev.36rus@gmail.com>
2020-12-04 18:52:06 +0300

    commit only changed files

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import reportWebVitals from './reportWebVitals';
-import state, {subscribe} from "./redux/State";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {addMessage, addPost, updateNewMessageText, updateNewPostText} from './redux/State'
+import store from './redux/Store'
+
 
 export let renderEntireTree = (state) => {
 
     ReactDOM.render (
         <React.StrictMode>
             <App state={state}
-                 updateNewPostText={updateNewPostText}
-                 addPost={addPost}
-                 updateNewMessageText={updateNewMessageText}
-                 addMessage={addMessage}/>
+                 addPost={store.addPost.bind(store)}
+                 updateNewPostText={store.updateNewPostText.bind(store)}
+                 addMessage={store.addMessage.bind(store)}
+                 updateNewMessageText={store.updateNewMessageText.bind(store)}
+            />
         </React.StrictMode>,
         document.getElementById('root')
     )
@@ -23,8 +24,8 @@ export let renderEntireTree = (state) => {
 
 
 
-renderEntireTree(state);
-subscribe(renderEntireTree);
+renderEntireTree(store.getState());
+store.subscribe(renderEntireTree);
 
 
 
